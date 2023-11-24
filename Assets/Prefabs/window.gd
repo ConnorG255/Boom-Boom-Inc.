e398e4cb6951e864d5548cf24cd18eb11c7ed519
conno <25connorgriffin@gmail.com>
2023-11-24 14:28:03 -0800

    Percent destroyed

--- Assets/Prefabs/window.gd
+++ Assets/Prefabs/window.gd
@@ -4,11 +4,14 @@ extends StaticBody2D
 
 @onready var winsad = $Winsad
 
+var notdestroyed = true
+
 
 
 func _on_area_2d_body_entered(body):
-	if body.is_in_group("explosive"):
+	if body.is_in_group("explosive") and notdestroyed:
 		winnormal.hide()
 		winsad.show()
-		Global.thingsLeft -= 1
+		Global.thingsLeft -= 1.00
+		notdestroyed = false
 	pass # Replace with function body.
