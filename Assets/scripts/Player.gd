84b0bbdf6ee548974221aa3125c32ddee97f6636
conno <25connorgriffin@gmail.com>
2023-11-24 19:12:11 -0800

    Anim

--- Assets/scripts/Player.gd
+++ Assets/scripts/Player.gd
@@ -1,13 +1,29 @@
 extends CharacterBody2D
+@onready var player = $Player
 
 @export var SPEED = 200;
+var upodown = true
+var direction = 0
+func anim():
+	if velocity != Vector2(0,0):
+		if upodown:
+			player.position.y += 0.5
+			await(get_tree().create_timer(0.1).timeout)
+			upodown = false
+		else:
+			player.position.y -= 0.5
+			await(get_tree().create_timer(0.1).timeout)
+			upodown = true
 
+		pass
 func movement():
-	var direction = Input.get_vector("A", "D", "W", "S");
+	direction = Input.get_vector("A", "D", "W", "S");
 	velocity = direction * SPEED;
+	
 	move_and_slide();
 	
 func _physics_process(delta):
 	movement();
+	anim()
 	
 
